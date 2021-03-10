090a78eadb8f3d49caee9e1fdc83b90c1cc8f4f7
Axel <axosar@gmail.com>
2021-03-09 18:32:25 -0600

    Resultados

--- pages/mapei/resultados.js
+++ pages/mapei/resultados.js
@@ -15,10 +15,23 @@ function Resultados() {
     <Layout>
       <div className="resultados">
         <div className="header">
-        <img src="/logo-deacero.svg" alt="Calculadoras para la construcción - Instituto Mexicano del Cemento y del Concreto A.C" style={{width: '300px'}} />
+          <img src="/logo-deacero.svg" alt="Calculadoras para la construcción - Instituto Mexicano del Cemento y del Concreto A.C" style={{width: '200px'}} />
           <h2>APP DE LOSAS PARA VIVIENDA</h2>
           <h3>LOSAS EN UNA DIRECCIÓN, SIMPLEMENTE APOYADAS</h3>
+          <hr/>
           <h2 className="naranja">RESULTADOS</h2>
+        </div>
+        <div className="datos">
+          <div>
+            <h2><span class="lnr lnr-chevron-right"></span> ÁREA: {area} mts<sup>2</sup></h2>
+            <h2><span class="lnr lnr-chevron-right"></span> DESTINO: {destino}</h2>
+            <h2><span class="lnr lnr-chevron-right"></span> LONGITUD DE CLARO: {claro} mt.</h2>
+          </div>
+          <div>
+            <h2><span class="lnr lnr-chevron-right"></span> PRECIO DEL CONCRETO: $ {pconcreto}</h2>
+            <h2><span class="lnr lnr-chevron-right"></span> PRECIO DE LA MALLA SOLDADA: $ {pmalla}</h2>
+            <h2><span class="lnr lnr-chevron-right"></span> PRECIO DE LA VARILLA G42: $ {pvar}</h2>
+          </div>
         </div>
         <div className="layt">
           <div>
