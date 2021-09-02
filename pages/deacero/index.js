b8799737e984a46d810ef5d83123d73042e403ec
Axel <axosar@gmail.com>
2021-09-02 10:48:47 -0500

    component structure

--- pages/deacero/index.js
+++ pages/deacero/index.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Layout from '../../components/Layout'
+import Layout from '../components/Layout'
 import styles from '../../styles/Mapei.module.css'
 import { TiArrowRight } from 'react-icons/ti';
 
