6dd0e31c1e2dc9682ff730b871c0adaeddbd7aa3
Axel <axosar@gmail.com>
2021-09-02 22:53:35 -0500

    tester

--- pages/deacero/resultados.js
+++ pages/deacero/resultados.js
@@ -13,6 +13,8 @@ function Resultados({ reasons }) {
   const [pconcreto, setPconcreto] = useState(0);
   const [pmalla, setPmalla] = useState(0);
   const [pvar, setPvar] = useState(0);
+  const [vigBovAA, setVigBovAA] = useState(0);
+  const [vigBovPretensada, setVigBovPretensada] = useState(0);
 
   useEffect(() => {
     setArea(localStorage.getItem('area'));
@@ -21,6 +23,10 @@ function Resultados({ reasons }) {
     setPconcreto(localStorage.getItem('pconcreto'));
     setPmalla(localStorage.getItem('pmalla'));
     setPvar(localStorage.getItem('pvar'));
+
+    setVigBovAA(reasons.filter(dato => dato.tipo == "Vig-Bov. A.A." && dato.claro == toString(claro) && dato.zona == destino));
+    setVigBovPretensada(reasons.filter(dato => dato.tipo == "Vig-Bov. Pretensada" && dato.claro == toString(claro) && dato.zona == destino));
+
   }, [area, destino, claro, pconcreto, pmalla, pvar]);
 
   function thousands_separators(num)
@@ -35,24 +41,6 @@ function Resultados({ reasons }) {
 
   return (
     <Layout>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/*reasons
-            .slice(0, reasons.length - 1)
-            .map(({ title, description, href }) => (
-              <a
-                className="border border-grey-200 rounded p-4 hover:shadow-lg hover:border-transparent"
-                key={title}
-                href={href}
-                target="_blank"
-              >
-                <h3 className="font-bold mb-2">{title}</h3>
-                <div dangerouslySetInnerHTML={{ __html: description }} />
-                <span className="text-blue-600 hover:text-blue-400 hover:underline mt-4 block">
-                  Documentation →
-                </span>
-              </a>
-            ))*/}
-        </div>
       <div className="resultados">
         <div className="header">
           <img src="/logo-deacero.svg" alt="Calculadoras para la construcción - Instituto Mexicano del Cemento y del Concreto A.C" style={{width: '200px'}} />
@@ -68,32 +56,30 @@ function Resultados({ reasons }) {
             <h2><span className="lnr lnr-chevron-right"></span> LONGITUD DE CLARO: {claro} mt.</h2>
           </div>
           <div>
-            <h2><span className="lnr lnr-chevron-right"></span> PRECIO DEL CONCRETO: $ {pconcreto}</h2>
-            <h2><span className="lnr lnr-chevron-right"></span> PRECIO DE LA MALLA SOLDADA: $ {pmalla}</h2>
-            <h2><span className="lnr lnr-chevron-right"></span> PRECIO DE LA VARILLA G42: $ {pvar}</h2>
+            <h2><span className="lnr lnr-chevron-right"></span> PRECIO DEL CONCRETO: $ {thousands_separators(pconcreto)}</h2>
+            <h2><span className="lnr lnr-chevron-right"></span> PRECIO DE LA MALLA SOLDADA: $ {thousands_separators(pmalla)}</h2>
+            <h2><span className="lnr lnr-chevron-right"></span> PRECIO DE LA VARILLA G42: $ {thousands_separators(pvar)}</h2>
           </div>
         </div>
         <div className="layt">
-          {/* 
           <Datos 
             bkg="uno"
             titulo="VIG-BOV Alma Abierta"
-            espesor={vigBovAA[0].espesor}
-            costo={vigBovAA[0].precio * 1.2}
-            costoTotal={thousands_separators(((vigBovAA[0].precio * 1.2) * area).toFixed(2))}
+            espesor={vigBovAA.espesor}
+            costo={vigBovAA.precio * 1.2}
+            costoTotal={thousands_separators(((vigBovAA.precio * 1.2) * area).toFixed(2))}
             cimbrado="1.6"
             ejecucion="4.9"
           />
           <Datos 
             bkg="dos"
             titulo="VIG-BOV Pretensada"
-            espesor={vigBovPretensada[0].espesor}
-            costo={vigBovPretensada[0].precio * 1.2}
-            costoTotal={thousands_separators(((vigBovPretensada[0].precio * 1.2) * area).toFixed(2))}
+            espesor={vigBovPretensada.espesor}
+            costo={vigBovPretensada.precio * 1.2}
+            costoTotal={thousands_separators(((vigBovPretensada.precio * 1.2) * area).toFixed(2))}
             cimbrado="1.6"
             ejecucion="5.2"
           />
-          */}
         </div>
         <div className="layt dos">
           {/* 
