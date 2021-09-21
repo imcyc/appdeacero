e8d033b5a5ce71e6e8217449ddcc52a2f0b5527b
Axel <axosar@gmail.com>
2021-09-21 08:43:23 -0500

    separadores

--- pages/components/datos.js
+++ pages/components/datos.js
@@ -7,23 +7,23 @@ function Datos({titulo, espesor, costo, costoTotal, cimbrado, ejecucion}) {
         <h2><span className="lnr lnr-arrow-right"></span> {titulo}</h2>
         <hr />
         <div className="datos">
-          <p className="esp"><span className="lnr lnr-arrow-right"></span> ESPESOR LOSA</p>
+          <p className="esp"><span className="lnr lnr-arrow-right"></span> ESPESOR LOSA:</p>
           <p>{espesor} cm</p>
         </div>
         <div className="datos">
-          <p className="esp"><span className="lnr lnr-arrow-right"></span> COSTO</p>
+          <p className="esp"><span className="lnr lnr-arrow-right"></span> COSTO:</p>
           <p>$ {costo} mt<sup>2</sup></p>
         </div>
         <div className="datos">
-          <p className="esp"><span className="lnr lnr-arrow-right"></span> COSTO TOTAL</p>
+          <p className="esp"><span className="lnr lnr-arrow-right"></span> COSTO TOTAL:</p>
           <p>$ {costoTotal}</p>
         </div>
         <div className="datos">
-          <p className="esp"><span className="lnr lnr-arrow-right"></span> CIMBRADO</p>
+          <p className="esp"><span className="lnr lnr-arrow-right"></span> CIMBRADO:</p>
           <p>{cimbrado} JOR</p>
         </div>
         <div className="datos">
-          <p className="esp"><span className="lnr lnr-arrow-right"></span> EJECUCIÓN TOTAL</p>
+          <p className="esp"><span className="lnr lnr-arrow-right"></span> EJECUCIÓN TOTAL:</p>
           <p>{ejecucion} JOR</p>
         </div>
         <div className="datos">
